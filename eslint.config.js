719488a2f42747cccdff9fdae4b141ccfe3ffedf
Hezekiah Michael <spiritomb@protonmail.com>
2025-01-22 16:00:32 -0500

    make a nice eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,30 @@
+import eslint from '@eslint/js';
+import tseslint from "typescript-eslint";
+import markdown from "eslint-plugin-markdown";
+
 export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...markdown.configs.recommended,
   {
-    "overrides": [{
-      "files": ["*.md"],
-      "parser": "eslint-plugin-markdownlint/parser",
-      "extends": ["plugin:markdownlint/recommended"]
-    }]
+    ignores: [
+      ".vitepress/cache/",
+      ".vitepress/dist/",
+    ],
+  },
+  {
+    plugins: {
+      markdown
+    }
+  },
+  {
+    files: ["src/**/*.md"],
+    processor: "markdown/markdown",
+  },
+  {
+    files: [".vitepress/config.ts"],
+    rules: {
+      "no-var": "off",
+    }
   }
 ];
-
