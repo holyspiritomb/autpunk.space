6888088cc67f5175541adad910c0e1948c114b29
Hezekiah Michael <github.cupel@aleeas.com>
2025-01-21 19:40:53 -0500

    Update config.ts
    add link to source repo

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -103,6 +103,7 @@ export default defineConfig({
       manualSortFileNameByPriority: ["fragments.md"]
     }),
     socialLinks: [
+      { icon: 'githun', ariaLabel: 'source repository for this site', 'https://github.com/holyspiritomb/autpunk.space' },
       { icon: 'github', ariaLabel: 'my github profile', link: 'https://github.com/holyspiritomb' },
       { icon: 'gitlab', ariaLabel: 'my gitlab profile', link: 'https://gitlab.com/holyspiritomb' },
     ],
