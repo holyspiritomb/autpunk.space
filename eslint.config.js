7172a9ddaa8e9d65ecadd2b1fa1f4861886d62a9
Hezekiah Michael <spiritomb@protonmail.com>
2025-01-23 18:21:45 -0500

    add eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,42 @@
+// @ts-check
+
 import eslint from '@eslint/js';
 import tseslint from "typescript-eslint";
-import markdown from "eslint-plugin-markdown";
+import markdown from "@eslint/markdown";
 
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 export default [
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...markdown.configs.recommended,
+  ...tseslint.configs.recommended.map(config => ({
+		...config,
+		files: ["**/*.ts"],
+	})),
   {
     ignores: [
       ".vitepress/cache/",
-      ".vitepress/dist/",
+      "LICENSE.md",
+      "dist/",
+      "src/public/"
     ],
   },
   {
+    files: ["src/**.md"],
     plugins: {
       markdown
+    },
+    processor: "markdown/markdown",
+    language: "markdown/commonmark",
+    rules: {
+      "no-irregular-whitespace": 0,
     }
   },
   {
-    files: ["src/**/*.md"],
+    files: ["README.md"],
+    plugins: {
+      markdown
+    },
     processor: "markdown/markdown",
+    language: "markdown/gfm",
   },
   {
     files: [".vitepress/config.ts"],
