b59fad7f17097e3a2ca562f8485a928e875cd9f2
Hezekiah Michael <spiritomb@protonmail.com>
2026-09-03 21:00:37 -0400

    update deps

--- uno.config.ts
+++ uno.config.ts
@@ -3,18 +3,19 @@ import {
   presetAttributify,
   presetIcons,
   presetTagify,
+  presetWind4,
   presetTypography,
   transformerDirectives,
   transformerVariantGroup,
 } from "unocss";
-import presetWind from "@unocss/preset-wind3";
-import { presetCatppuccin } from "unocss-preset-catppuccin";
+import presetCatppuccin from "@catppuccin/unocss";
 
 export default defineConfig({
   content: {
     filesystem: ["**/*.{html,js,ts,vue,scss,css}"],
   },
   presets: [
+    presetWind4(),
     presetCatppuccin({
       defaultFlavour: "mocha",
     }),
@@ -32,11 +33,10 @@ export default defineConfig({
       },
     }),
     presetTagify(),
-    presetWind(),
     presetTypography(),
   ],
   shortcuts: {
-    "btn": "rounded-full shadow-md py-2 px-4 bg-ctp-latte-pink dark:bg-ctp-latte-pink font-bold border-ctp-pink outline-ctp-pink outline-1 shadow-ctp-pink m-[0.5rem] h-[3rem] active:shadow-lg border-1 p-3",
+    "btn": "rounded-full shadow-md py-2 px-4 font-bold border-ctp-pink outline-ctp-pink outline-1 shadow-ctp-pink m-[0.5rem] h-[3rem] active:shadow-lg border-1 p-3",
     "input": "py-2 px-4 rounded-sm shadow-md focus:shadow-lg focus:shadow-ctp-sky shadow-ctp-sky py-2 px-4 border-1 border-pink-400 focus:border-pink-700 bg-pink-100 dark:bg-gray-700/50 dark:shadow-ctp-sky caret-pink dark:caret-ctp-sky m-[0.5rem] h-[3rem] focus:outline-1 focus:outline-blue-400",
     "label": "py-2 px-4",
   },
